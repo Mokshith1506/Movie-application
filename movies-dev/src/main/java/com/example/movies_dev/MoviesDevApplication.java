49b76519336f4936efd5a60391f62277aa0cffa5
Mokshith1506 <mokshith0123@gmail.com>
2025-08-08 08:13:05 +0530

    Initial commit

---
+++ movies-dev/src/main/java/com/example/movies_dev/MoviesDevApplication.java
@@ -0,0 +1,13 @@
+package com.example.movies_dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesDevApplication.class, args);
+	}
+
+}
---
+++ movies-dev/src/test/java/com/example/movies_dev/MoviesDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.movies_dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
